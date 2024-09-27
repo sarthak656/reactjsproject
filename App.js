2953436958b1bdbc318935dcafd27f0bd075bf12
Sarthak Shetty <sarthak.shetty@kambala.co.in>
2024-09-27 10:19:00 +0530

    Laying the foundation

--- App.js
+++ App.js
@@ -1,5 +1,20 @@
+import React from 'react';    
+ import ReactDOM from 'react-dom/client';
 
-const parent = React.createElement("div",{id:"parent"},
-React.createElement("div",{id:"child"},[React.createElement("h1",{id:"h1"},"h1tag"),React.createElement("h2",{id:"h2"},"h2 tag dude")]))
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+  //JSX-it is html like syntax 
+  const jsxheading = (<h1 className='head'>sarthak</h1>) ;  //React Element
+  var a = 12;
+
+const NewHeading = () => (
+ <div>lets hack</div>
+)
+    const HeadingComponent = () => (    //React Functional component
+         <div id="container">
+       <h1>Welcome</h1>
+       <NewHeading />  
+       {a}
+       </div>
+    );
+
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(<HeadingComponent />);   //rendering normal functional component
