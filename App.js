50d69391bf4a956ba2ec02155a5d7e3403d94e5e
Sarthak Shetty <sarthak.shetty@kambala.co.in>
2024-09-27 12:18:32 +0530

    coding Assignment in Assignment3

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import React from 'react';    
- import ReactDOM from 'react-dom/client';
+import React from "react";
+import  ReactDOM  from "react-dom/client";
 
-  //JSX-it is html like syntax 
-  const jsxheading = (<h1 className='head'>sarthak</h1>) ;  //React Element
-  var a = 12;
+//using React.createElement
+// const elem1 = React.createElement("h1",{className: "bgcolors"},"User1");
+// const elem2 = React.createElement("h2",{className:"bgcolors"},"User2");
+// const elem3 = React.createElement("h3",{className:"bgcolors"},"User3");
 
-const NewHeading = () => (
- <div>lets hack</div>
-)
-    const HeadingComponent = () => (    //React Functional component
-         <div id="container">
-       <h1>Welcome</h1>
-       <NewHeading />  
-       {a}
-       </div>
-    );
+//same using JSX
+//  const elements = (
+//   <div>
+//  <h1>User1</h1>
+// <h2>User2</h2>
+// <h3>User3</h3>
+// </div>
+//  )
 
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(<HeadingComponent />);   //rendering normal functional component
+// same using functional component
+ const JsxHeadings = () => {
+  return <div>
+     <h1>User1</h1>
+     <h2>User2</h2>
+     <h3>User3</h3>
+  </div>
+ } 
+   
+ // Pass attributes into the tag in JSX
+ //Composition of Component(Add a component inside another)
+ const JsxHeadings2 = () =>{
+  return (
+    <div>
+      <div className="bgcolors">welcome home</div> 
+      <JsxHeadings />
+    </div>
+  );
+ }
+
+ const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render("div",null,elem1,elem2,elem3);
+ //root.render(elements);
+ root.render(<JsxHeadings2 />)
